734bfb41254c59440d407e6adbc550095a361675
abdulahi roble <abdulahi_2610@hotmail.com>
2024-05-29 21:45:55 +0200

    Add carousel

--- customize-app/components/layout/sections/NavigationSection.js
+++ customize-app/components/layout/sections/NavigationSection.js
@@ -1,4 +1,4 @@
-import LineChart from "../../LineChart"
+import Listings from "../../Listings"
 import { Tooltip } from '@chakra-ui/react';
 import { QuestionIcon } from '@chakra-ui/icons'
 
@@ -8,13 +8,14 @@ const NavigationSection = (props) => (
       htmlFor="user-control-navigation"
       className="text-sm font-bold dark:text-twitterAccentOneDark text-twitterAccentOne"
     >
-      Traffic <Tooltip label="Shows the website's traffic changes over the last six months.
+      Listing <Tooltip label="Shows the website's traffic changes over the last six months.
       " fontSize="md" placement='right-start'><QuestionIcon fontSize="sm" mt={-1} /></Tooltip>
     </label>
     <div id="user-control-navigation">
       {/* <Navigation /> */}
       {/* <Line /> */}
-      <LineChart monthAndYear={props.props.props?.monthAndYear} traffic={props.props.props?.traffic} />
+      <Listings />
+      {/* <LineChart monthAndYear={props.props.props?.monthAndYear} traffic={props.props.props?.traffic} /> */}
     </div>
   </section>
 )
